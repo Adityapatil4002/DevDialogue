4262fd39b145f8c8f8bf45f067e7a89f0cb8a5cc
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-10-22 10:29:43 +0530

    Added project endpoints so that the project creation and database schema become easy to handle

---
+++ Backend/Controllers/project.controller.js
@@ -0,0 +1,32 @@
+import projectModel from '../Models/project.model.js';
+import projectService from '../Services/project.service.js';
+import userModel from '../Models/user.model.js';
+import {validationResult} from 'express-validator';
+
+
+export const createProject = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    try { 
+    const { name } = req.body;
+    const loggedInUser = await userModel.findOne({ email: req.user.email });
+
+    const userId = loggedInUser._id;
+
+    const newProject = await projectService.createProject({
+        name,
+        userId
+    })
+
+    res.status(201).json(newProject);
+    } catch (err) {
+        res.status(400).send(err.message);
+        console.log(err);
+    
+}
+
+    
+}
---
+++ Backend/Services/project.service.js
@@ -0,0 +1,23 @@
+import projectModel from '../Models/project.model.js';
+
+export const createProject = async ({
+    name,
+    userId
+}) => {
+    if (!name) {
+        throw new Error('Project name is required');
+    
+    }
+    if (!userId) {
+        throw new Error('User id is required');
+
+    }
+
+    const project = await projectModel.create({
+        name,
+        users: [userId]
+    
+    })
+
+    return project;
+}
