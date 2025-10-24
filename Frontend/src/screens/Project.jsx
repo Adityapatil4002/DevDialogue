8f9cc81deb8f44147f01b0a2b4b4fa1ea890bc0d
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-10-24 18:40:30 +0530

    Created a users sidepanel structure

--- Frontend/src/screens/Project.jsx
+++ Frontend/src/screens/Project.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 const Project = () => {
     
     const location = useLocation();
+
+    const [isSidePanelOpen, setisSidePanelOpen] = useState(false);
+
     console.log(location.state);
 
   return (
     <main className="h-screen w-screen flex">
-      <section className="left flex flex-col h-full min-w-80 bg-slate-300">
+      <section className="left relative flex flex-col h-full min-w-80 bg-slate-300">
         <header className="flex justify-end p-2 px-4 w-full bg-slate-100">
-          <button className="p-2 ">
+                  <button
+                      onClick={() => setisSidePanelOpen(!isSidePanelOpen)}
+                      lassName="p-2 ">
             <i className="ri-group-fill"></i>
           </button>
         </header>
@@ -39,6 +44,18 @@ const Project = () => {
               <i className="ri-send-plane-fill"></i>
             </button>
           </div>
+        </div>
+
+        <div
+            className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all ${isSidePanelOpen ? 'translate-x-0' : 'translate-x-full'} top-0`}>
+                  <header
+                  className='flex justify-end px-3 p-2 bg-slate-200 '>
+                      <button
+                      onClick={() => setisSidePanelOpen(false)}
+                      className='p-2 '>
+                          <i className='ri-close-fill'></i>
+                      </button>
+                  </header>  
         </div>
       </section>
     </main>
