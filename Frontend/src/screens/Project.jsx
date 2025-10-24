9eea6f1e46b72b23b6d2e3f3619407360cd406f5
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-10-24 18:17:27 +0530

    Made a chat like structure

--- Frontend/src/screens/Project.jsx
+++ Frontend/src/screens/Project.jsx
@@ -2,18 +2,47 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
-const Project = ({  }) => {
+const Project = () => {
     
     const location = useLocation();
-
-    const navigate = useNavigate();
-
-    console.log(navigate)
-
+    console.log(location.state);
 
   return (
-    <div>Project</div>
-  )
+    <main className="h-screen w-screen flex">
+      <section className="left flex flex-col h-full min-w-80 bg-slate-300">
+        <header className="flex justify-end p-2 px-4 w-full bg-slate-100">
+          <button className="p-2 ">
+            <i className="ri-group-fill"></i>
+          </button>
+        </header>
+
+        <div className="conversation-area flex-grow flex flex-col">
+          <div className="message-box p-1 flex-grow flex flex-col gap-1">
+            <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-sm">example@gmail.com</small>
+              <p className="test-sm">
+                ipsum dolor sit amet.ipsum dolor sit amet.
+              </p>
+            </div>
+            <div className="ml-auto max-w-56 message flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+              <small className="opacity-65 text-sm">example@gmail.com</small>
+              <p className="test-sm">ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div className="inputField w-full flex ">
+            <input
+              className="p-2 px-4 border-none outline-none"
+              type="text"
+              placeholder="Enter message"
+            />
+            <button className="flex-grow px-3">
+              <i className="ri-send-plane-fill"></i>
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default Project
