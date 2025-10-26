be8adf6a2deb1ca0dfdab2151ded28b33d00c5d5
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-10-26 14:23:34 +0530

    socket is not setup on the server side and it can now be successfully connect through the authorization (through token)

--- Backend/server.js
+++ Backend/server.js
@@ -3,23 +3,44 @@ dotenv.config();
 import http from "http";
 import app from "./app.js";
 import connect from "./db/db.js";
+import {Server} from "socket.io";
+import jwt from "jsonwebtoken";
 
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const io = new Server(server);
+
+io.use((socket, next) => {
+  try {
+    const token = socket.handshake.auth?.token || socket.handshake.headers?.authorization?.split(" ")[1];
+    if (!token) {
+      return next(new Error("Authentication error: Token not provided"));
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if(!decoded || !decoded._id) {
+      return next(new Error("Authentication error: Invalid token"));
+    }
+    socket.user = decoded;
+    next();
+
+  } catch (error) {
+    next(error);
+    
+  }
+})
 
 io.on('connection', socket => {
 
   console.log('New user connected');
 
-  client.on('event', data => { });
-  client.on('disconnect', () => { });
+  socket.on('event', data => { });
+  socket.on('disconnect', () => { });
 });
 
 
-
 server.listen(port, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${port}`);
 });
