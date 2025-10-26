4a67898211750b645ccd79dfaa79cbc30972dcb7
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-10-26 13:58:25 +0530

    The basic socket io structure setup is now complete

--- Backend/server.js
+++ Backend/server.js
@@ -8,6 +8,17 @@ import connect from "./db/db.js";
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+io.on('connection', socket => {
+
+  console.log('New user connected');
+
+  client.on('event', data => { });
+  client.on('disconnect', () => { });
+});
+
+
 
 server.listen(port, () => {
   console.log("Server is running on port 3000");
