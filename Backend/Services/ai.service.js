6262eb2dbe7b76eaebc52c6fb7b10d47cd7b48df
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-11-07 14:33:38 +0530

    made some changes so that Ai will give answers like a experince dev

--- Backend/Services/ai.service.js
+++ Backend/Services/ai.service.js
@@ -1,7 +1,19 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
+const model = genAI.getGenerativeModel({
+    model: "gemini-2.5-flash",
+    systemInstructions: `You are an expert in MERN, developement, 
+    DSA and Ai technologies, you have an experince of 10 years in
+    the developement. You always write a code in modular and break
+    the code in the possible way and follow best practicaes, you
+    use understandable comments in the code, you create files as
+    needed, you write code while maintaining the working of
+    previous code. you always follow and best practices of the
+    developement you never miss the edge cases and always write
+    code that is scalable and maintainable, in your code you
+    always handle the errors and exceptions properly.`,
+});
 
 export const generateResult = async (prompt) => {
     const result = await model.generateContent(prompt);
