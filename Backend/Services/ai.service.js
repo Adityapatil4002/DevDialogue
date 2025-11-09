c465415362461617cf27c77674900834da5990d5
Adityapatil4002 <patiladitya6062004@gmail.com>
2025-11-09 11:19:37 +0530

    improve the system instruction for getting desired respinse form AI

--- Backend/Services/ai.service.js
+++ Backend/Services/ai.service.js
@@ -2,7 +2,10 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+    model: "gemini-2.5-flash",
+    generationConfig: {
+        responseMimeType: "application/json",
+        
   systemInstruction: `You are an expert in MERN, developement, 
     DSA and Ai technologies, you have an experince of 10 years in
     the developement. You always write a code in modular and break
@@ -12,7 +15,55 @@ const model = genAI.getGenerativeModel({
     previous code. you always follow and best practices of the
     developement you never miss the edge cases and always write
     code that is scalable and maintainable, in your code you
-    always handle the errors and exceptions properly.`,
+    always handle the errors and exceptions properly.
+    You always write code in the way that it is easy to read and
+
+    Examples:
+
+    <example>
+    user: Create an express application
+    response: {
+        "text": "This is your file tree structure of the express server."
+        "filetree": {
+        "app.js": {
+            content: "
+            import express from 'express';
+            const app = express();
+
+            app.get('/', (req, res) => {
+            res.send('Hello World!');
+            });
+            app.listen(3000, () => {
+            console.log('Server listening on port 3000!');
+            });
+            "
+        },
+        "package.json": {
+            content: "
+                "name": "express-app",
+                "version": "1.0.0",
+                "main": "app.js",
+                "scripts": {
+                "start": "node app.js"
+                },
+                "keywords": [],
+                "author": "",
+                "license": "ISC",
+                "description": "A simple Express application",
+                "dependencies": {
+                "express": "^4.17.1"
+                }
+            "
+        }
+
+        }
+
+        }
+    }
+</example>
+
+`
+    }
 });
 
 export const generateResult = async (prompt) => {
